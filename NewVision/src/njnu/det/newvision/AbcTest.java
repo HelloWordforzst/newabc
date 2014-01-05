0c54a1a53253799129899380a6c8364e9eed085e
zst <zs_t@sina.cn>
2014-01-05 20:07:48 +0800

    abctest

--- NewVision/src/njnu/det/newvision/AbcTest.java
+++ NewVision/src/njnu/det/newvision/AbcTest.java
@@ -2,12 +2,14 @@ package njnu.det.newvision;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.format.Time;
 
 public class AbcTest extends Activity{
    @Override
 protected void onCreate(Bundle savedInstanceState) {
 	// TODO Auto-generated method stub
 	super.onCreate(savedInstanceState);
-	abcdedfddfjdkfdkfdkfdfdfdfdfdf
+	
+//能传上去吗？        222222
 }
 }
